62724d5c7c1ad371a50440f6d738309e7c6294b8
deron19 <161203756+deron19@users.noreply.github.com>
2025-01-07 18:37:48 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,92 @@
+(function() {
+	'use strict';
+
+	var tinyslider = function() {
+		var el = document.querySelectorAll('.testimonial-slider');
+
+		if (el.length > 0) {
+			var slider = tns({
+				container: '.testimonial-slider',
+				items: 1,
+				axis: "horizontal",
+				controlsContainer: "#testimonial-nav",
+				swipeAngle: false,
+				speed: 700,
+				nav: true,
+				controls: true,
+				autoplay: true,
+				autoplayHoverPause: true,
+				autoplayTimeout: 3500,
+				autoplayButtonOutput: false
+			});
+		}
+	};
+	tinyslider();
+
+	
+
+
+	var sitePlusMinus = function() {
+
+		var value,
+    		quantity = document.getElementsByClassName('quantity-container');
+
+		function createBindings(quantityContainer) {
+	      var quantityAmount = quantityContainer.getElementsByClassName('quantity-amount')[0];
+	      var increase = quantityContainer.getElementsByClassName('increase')[0];
+	      var decrease = quantityContainer.getElementsByClassName('decrease')[0];
+	      increase.addEventListener('click', function (e) { increaseValue(e, quantityAmount); });
+	      decrease.addEventListener('click', function (e) { decreaseValue(e, quantityAmount); });
+	    }
+
+	    function init() {
+	        for (var i = 0; i < quantity.length; i++ ) {
+						createBindings(quantity[i]);
+	        }
+	    };
+
+	    function increaseValue(event, quantityAmount) {
+	        value = parseInt(quantityAmount.value, 10);
+
+	        console.log(quantityAmount, quantityAmount.value);
+
+	        value = isNaN(value) ? 0 : value;
+	        value++;
+	        quantityAmount.value = value;
+	    }
+
+	    function decreaseValue(event, quantityAmount) {
+	        value = parseInt(quantityAmount.value, 10);
+
+	        value = isNaN(value) ? 0 : value;
+	        if (value > 0) value--;
+
+	        quantityAmount.value = value;
+	    }
+	    
+	    init();
+		
+	};
+	sitePlusMinus();
+
+	document.addEventListener('DOMContentLoaded', function () {
+	  // Custom JavaScript code here
+
+	  // Example: Smooth scroll for anchor links
+	  const links = document.querySelectorAll('a[href^="#"]');
+	  for (const link of links) {
+	    link.addEventListener('click', function (e) {
+	      e.preventDefault();
+	      const targetId = this.getAttribute('href').substring(1);
+	      const targetElement = document.getElementById(targetId);
+	      if (targetElement) {
+	        window.scrollTo({
+	          top: targetElement.offsetTop,
+	          behavior: 'smooth'
+	        });
+	      }
+	    });
+	  }
+	});
+
+})()
